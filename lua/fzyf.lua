be37de6f5f8929fd38d17916c4ae6b35c2492b34
timofey basuev <echiauta@gmail.com>
2025-08-15 11:52:40 +0500

    refactor: maybe improve perf and some qol

--- lua/fzyf.lua
+++ lua/fzyf.lua
@@ -1,73 +1,85 @@
---- @param cmd string
---- @param tempf string
-local function open_terminal(cmd, tempf)
-	local status = vim.fn.termopen(cmd .. tempf, {
-		on_exit = function()
-			vim.api.nvim_command("bd!")
-			local f = io.open(tempf, "r")
-			if f == nil then
-				return
-			end
-			local stdout = f:read("l")
-			vim.api.nvim_command("e " .. stdout)
-		end,
-	})
-	if status == -1 or status == 0 then
-		error("failed to open terminal")
-	end
-end
+local M = {}
 
--- TODO: implement window configuration
 local function spawn_float()
-	local width = vim.o.columns - 30
-	local height = vim.o.lines - 10
-	vim.api.nvim_open_win(vim.api.nvim_create_buf(false, true), true, {
+	local cols, lines = vim.o.columns, vim.o.lines
+	local width = math.max(40, cols - 30)
+	local height = math.max(10, lines - 10)
+	local buf = vim.api.nvim_create_buf(false, true)
+	vim.bo[buf].bufhidden = "wipe"
+	vim.bo[buf].swapfile = false
+	vim.api.nvim_open_win(buf, true, {
 		relative = "editor",
 		style = "minimal",
 		width = width,
 		height = height,
-		col = math.min((vim.o.columns - width) / 2),
-		row = math.min((vim.o.lines - height) / 2),
+		col = math.floor((cols - width) / 2),
+		row = math.floor((lines - height) / 2),
+		border = "none",
 	})
 end
 
---- @param cmd string
-local function execute_cmd(cmd)
+local function open_terminal(pipeline, tempf)
+	local status = vim.fn.termopen(pipeline .. " > " .. vim.fn.shellescape(tempf), {
+		on_exit = function()
+			vim.api.nvim_command("bd!")
+			local f = io.open(tempf, "r")
+			if not f then return end
+			local out = f:read("*l")
+			f:close()
+			pcall(vim.loop.fs_unlink, tempf)
+			if not out or out == "" then return end
+			local lnum, file = out:match("^%+(%d+)%s+(.+)$")
+			if file then
+				vim.cmd(string.format("edit +%s %s", lnum, vim.fn.fnameescape(file)))
+			else
+				vim.cmd("edit " .. vim.fn.fnameescape(out))
+			end
+		end,
+	})
+	if not status or status <= 0 then
+		error("termopen failed")
+	end
+end
+
+local function execute_pipeline(pipeline)
 	spawn_float()
-	vim.api.nvim_command("startinsert")
-	local tempf = vim.fn.tempname()
-	open_terminal(cmd, tempf)
+	vim.cmd("startinsert")
+	local tmp = vim.fn.tempname()
+	open_terminal(pipeline, tmp)
+end
+
+local function fzy_list_height()
+	return math.max(10, vim.o.lines - 10)
 end
 
 local function find_file()
-	local cmd = "fd -tf -cnever . | fzy -l" .. vim.o.lines - 10 .. " > "
-	execute_cmd(cmd)
+	local pipeline = string.format("fd -tf --color=never . | fzy -l %d", fzy_list_height())
+	execute_pipeline(pipeline)
 end
 
 local function lookup_config()
 	local cfgdir = vim.fn.stdpath("config")
-	local cmd = "fd -tf -cnever . " .. cfgdir .. " | fzy -l" .. vim.o.lines - 10 .. " > "
-	execute_cmd(cmd)
+	local pipeline = string.format(
+		"fd -tf --color=never . %s | fzy -l %d",
+		vim.fn.shellescape(cfgdir),
+		fzy_list_height()
+	)
+	execute_pipeline(pipeline)
 end
 
 local function live_grep()
-	-- TODO: improve livegrep cmd
-	local cmd = "rg -i --vimgrep . | awk -F':' '!seen[$1\":\"$2]++' | fzy -l25 | awk -F':' '{print \"+\"$2, $1}' > "
-	execute_cmd(cmd)
+	local rg = table.concat({
+		"rg --vimgrep --no-heading --color=never --smart-case .",
+		"awk -F':' '!seen[$1\":\"$2]++ {printf \"+%s %s\n\", $2, $1}'",
+	}, " | ")
+	local pipeline = string.format("%s | fzy -l %d", rg, fzy_list_height())
+	execute_pipeline(pipeline)
 end
 
-local M = {}
-
 function M.setup()
-	vim.api.nvim_create_user_command("FzyfFindFile", function()
-		find_file()
-	end, {})
-	vim.api.nvim_create_user_command("FzyfLiveGrep", function()
-		live_grep()
-	end, {})
-	vim.api.nvim_create_user_command("FzyfLookupConfig", function()
-		lookup_config()
-	end, {})
+	vim.api.nvim_create_user_command("FzyfFindFile", find_file, {})
+	vim.api.nvim_create_user_command("FzyfLiveGrep", live_grep, {})
+	vim.api.nvim_create_user_command("FzyfLookupConfig", lookup_config, {})
 end
 
 return M
