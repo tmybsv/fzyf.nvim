a1b1f6634676224a6900c345bcb8202cdc2f6880
timofey basuev <echiauta@gmail.com>
2025-08-15 12:04:49 +0500

    chore: revert

--- lua/fzyf.lua
+++ lua/fzyf.lua
@@ -1,25 +1,7 @@
-local M = {}
-
-local function spawn_float()
-	local cols, lines = vim.o.columns, vim.o.lines
-	local width = math.max(40, cols - 30)
-	local height = math.max(10, lines - 10)
-	local buf = vim.api.nvim_create_buf(false, true)
-	vim.bo[buf].bufhidden = "wipe"
-	vim.bo[buf].swapfile = false
-	vim.api.nvim_open_win(buf, true, {
-		relative = "editor",
-		style = "minimal",
-		width = width,
-		height = height,
-		col = math.floor((cols - width) / 2),
-		row = math.floor((lines - height) / 2),
-		border = "none",
-	})
-end
-
-local function open_terminal(pipeline, tempf)
-	local status = vim.fn.termopen(pipeline .. " > " .. vim.fn.shellescape(tempf), {
+--- @param cmd string
+--- @param tempf string
+local function open_terminal(cmd, tempf)
+	local status = vim.fn.termopen(cmd .. " > " .. vim.fn.shellescape(tempf), {
 		on_exit = function()
 			vim.api.nvim_command("bd!")
 			local f = io.open(tempf, "r")
@@ -41,46 +23,62 @@ local function open_terminal(pipeline, tempf)
 	end
 end
 
-local function execute_pipeline(pipeline)
-	spawn_float()
-	vim.cmd("startinsert")
-	local tmp = vim.fn.tempname()
-	open_terminal(pipeline, tmp)
+-- TODO: implement window configuration
+local function spawn_float()
+	local cols, lines = vim.o.columns, vim.o.lines
+	local width = math.max(40, cols - 30)
+	local height = math.max(10, lines - 10)
+	local buf = vim.api.nvim_create_buf(false, true)
+	vim.bo[buf].bufhidden = "wipe"
+	vim.bo[buf].swapfile = false
+	vim.api.nvim_open_win(buf, true, {
+		relative = "editor",
+		style = "minimal",
+		width = width,
+		height = height,
+		col = math.floor((cols - width) / 2),
+		row = math.floor((lines - height) / 2),
+		border = "none",
+	})
 end
 
-local function fzy_list_height()
-	return math.max(10, vim.o.lines - 10)
+--- @param cmd string
+local function execute_cmd(cmd)
+	spawn_float()
+	vim.api.nvim_command("startinsert")
+	local tempf = vim.fn.tempname()
+	open_terminal(cmd, tempf)
 end
 
 local function find_file()
-	local pipeline = string.format("fd -tf --color=never . | fzy -l %d", fzy_list_height())
-	execute_pipeline(pipeline)
+	local cmd = "fd -tf -cnever . | fzy -l" .. vim.o.lines - 10 .. " > "
+	execute_cmd(cmd)
 end
 
 local function lookup_config()
 	local cfgdir = vim.fn.stdpath("config")
-	local pipeline = string.format(
-		"fd -tf --color=never . %s | fzy -l %d",
-		vim.fn.shellescape(cfgdir),
-		fzy_list_height()
-	)
-	execute_pipeline(pipeline)
+	local cmd = "fd -tf -cnever . " .. cfgdir .. " | fzy -l" .. vim.o.lines - 10 .. " > "
+	execute_cmd(cmd)
 end
 
 local function live_grep()
-	local rg = table.concat({
-		"rg --vimgrep --no-heading --color=never --smart-case .",
-		[[awk -F':' '!seen[$1":"$2]++ {printf "+%s %s
-", $2, $1}']]
-	}, " | ")
-	local pipeline = string.format("%s | fzy -l %d", rg, fzy_list_height())
-	execute_pipeline(pipeline)
+	-- TODO: improve livegrep cmd
+	local cmd = "rg -i --vimgrep . | awk -F':' '!seen[$1\":\"$2]++' | fzy -l25 | awk -F':' '{print \"+\"$2, $1}' > "
+	execute_cmd(cmd)
 end
 
+local M = {}
+
 function M.setup()
-	vim.api.nvim_create_user_command("FzyfFindFile", find_file, {})
-	vim.api.nvim_create_user_command("FzyfLiveGrep", live_grep, {})
-	vim.api.nvim_create_user_command("FzyfLookupConfig", lookup_config, {})
+	vim.api.nvim_create_user_command("FzyfFindFile", function()
+		find_file()
+	end, {})
+	vim.api.nvim_create_user_command("FzyfLiveGrep", function()
+		live_grep()
+	end, {})
+	vim.api.nvim_create_user_command("FzyfLookupConfig", function()
+		lookup_config()
+	end, {})
 end
 
 return M
